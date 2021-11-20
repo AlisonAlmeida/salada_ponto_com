00ede272f2b4f9accd264fa0da93c2c27956cbf5
AlisonAlmeida <alison.asa@gmail.com>
2021-11-20 02:15:45 -0400

    a

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,67 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
 
-import 'ui/input_field.dart';
+import 'request_item.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
-  LoginPageState createState() => LoginPageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage> {
+  @override
+  void initState() {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.white));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('lib/assets/imgs/background_home_page.jpg'),
-              fit: BoxFit.cover),
-        ),
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Form(
-              child: InputField(
-                  const Icon(
-                    Icons.person,
-                    color: Colors.white,
-                  ),
-                  'Usuario'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            width: 500,
+            decoration: const BoxDecoration(
+              color: Colors.white,
             ),
-            Form(
-              child: InputField(
-                  const Icon(
-                    Icons.lock,
-                    color: Colors.white,
-                  ),
-                  'Senha'),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: ElevatedButton.icon(
-                label: const Text(
-                  'Realizar Pedido',
-                  style: TextStyle(
-                    fontFamily: 'IrishGrover',
-                    fontSize: 30,
-                  ),
-                ),
-                icon: const Icon(Icons.arrow_right_rounded),
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.blueGrey,
-                    fixedSize: const Size(0, 70),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50))),
+            child: const Text(
+              'Salada.com',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'IrishGrover',
+                color: Colors.blueGrey,
+                fontSize: 60,
               ),
             ),
-          ],
-        ),
+          ),
+          
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: ElevatedButton.icon(
+              label: const Text(
+                'Realizar Pedido',
+                style: TextStyle(
+                  fontFamily: 'IrishGrover',
+                  fontSize: 30,
+                ),
+              ),
+              icon: const Icon(Icons.arrow_right_rounded),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const RequestPage()));
+              },
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.blueGrey,
+                  fixedSize: const Size(0, 70),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50))),
+            ),
+          ),
+        ],
       ),
     );
   }
